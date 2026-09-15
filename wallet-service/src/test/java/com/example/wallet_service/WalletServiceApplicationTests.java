de72c1b14cf29a118361243333c614442f0863e5
Anjanimhatre <114746327+Anjanimhatre@users.noreply.github.com>
2026-09-15 19:44:35 +0530

    Initial wallet service setup with H2

--- wallet-service/src/test/java/com/example/wallet_service/WalletServiceApplicationTests.java
+++ wallet-service/src/test/java/com/example/wallet_service/WalletServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.wallet_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class WalletServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
